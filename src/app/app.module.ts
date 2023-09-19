676159fbf959eb0c70c1419823c8795b20a82038
enzomedina <enzomedinacoder@gmail.com>
2023-09-19 00:10:41 -0300

    Se inicio lista de Alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
+
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    StudentsComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+],
   imports: [
     BrowserModule,
     AppRoutingModule
@@ -15,4 +23,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }                                      
